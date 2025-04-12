e17feee9d92d9fdb6d0e5ba375d017d98369dcda
NourhanAlsaid <nalsaid@students.kennesaw.edu>
2025-04-12 12:08:42 -0400

    Update Nourhan-create_Events.py

--- Nourhan-create_Events.py
+++ Nourhan-create_Events.py
@@ -10,19 +10,19 @@ while True:
         pygame.quit()
         sys.exit(0)
         
-def move (player,keys):
+def move (Snake,keys):
    if key[K_w]: 
-      if player.rect.top > 0:
-       player_rect = player.rect.move (0,-12) # move the player 
+      if Snake.rect.top > 0:
+       Snake_rect = Snake.rect.move (0,-12) 
     if keys[K_s] 
-        if player.rect.bottom > (800): 
-        player_rect = player.rect.move (0,12) # move the player
+        if Snake.rect.bottom > (800): 
+        Snake_rect = Snake.rect.move (0,12) 
     if keys[K_a] 
-        if player.rect.left > 0:
-        player_rect =  player.rect.move (-12,0)
+        if Snake.rect.left > 0:
+        Snake_rect =  Snake.rect.move (-12,0)
     if keys[K_d]:
-        if player.rect.right < (800):
-        player_rect = player.rect.move (12,0)
+        if Snake.rect.right < (800):
+        Snake_rect = Snake.rect.move (12,0)
           
           
            
